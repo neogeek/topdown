60cf3ed20ab2368a460d2a0e07f2ff8734d96c1b
Scott Doxey <neogeek0101@gmail.com>
2018-05-29 22:19:53 -0400

    Added authentication step.

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -3,24 +3,42 @@ import ReactDOM from 'react-dom';
 
 import { Board, List, Card } from './components';
 
-import { getAllData } from './utilities/app';
+import {
+    authorize,
+    getAllData,
+    getTokenFromUrl,
+    userToken
+} from './utilities/app';
 
-getAllData().then(data => {
+const tokenFromURL = getTokenFromUrl();
+
+if (tokenFromURL) {
+    localStorage.setItem('trello_token', tokenFromURL);
+}
+
+if (userToken()) {
+    getAllData().then(data => {
+        ReactDOM.render(
+            <Board>
+                {data.map((list, i) => (
+                    <List name={list.name} key={i}>
+                        {list.cards.map((card, j) => (
+                            <Card
+                                name={card.name}
+                                url={card.url}
+                                boardName={card.boardName}
+                                key={j}
+                            />
+                        ))}
+                    </List>
+                ))}
+            </Board>,
+            document.getElementById('root')
+        );
+    });
+} else {
     ReactDOM.render(
-        <Board>
-            {data.map((list, i) => (
-                <List name={list.name} key={i}>
-                    {list.cards.map((card, j) => (
-                        <Card
-                            name={card.name}
-                            url={card.url}
-                            boardName={card.boardName}
-                            key={j}
-                        />
-                    ))}
-                </List>
-            ))}
-        </Board>,
+        <button onClick={authorize}>Login with Trello</button>,
         document.getElementById('root')
     );
-});
+}
