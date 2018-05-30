21a502cf3c9f3cd07207fb248e946058b21c91ff
Scott Doxey <neogeek0101@gmail.com>
2018-05-29 22:18:27 -0400

    Added appName and redirectUrl.

--- src/js/utilities/app.js
+++ src/js/utilities/app.js
@@ -1,4 +1,6 @@
+const appName = 'Top Down';
 const appKey = '6a26fcd8d47e93922b8caaab9178aa18';
+const redirectUrl = `${window.location.origin}${window.location.pathname}`;
 const userToken = () => localStorage.getItem('trello_token');
 
 const apiRequest = path => {
