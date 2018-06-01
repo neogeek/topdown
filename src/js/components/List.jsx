0659445b0595c1ab824877aa6612603d75552845
Scott Doxey <neogeek0101@gmail.com>
2018-05-31 21:54:56 -0400

    Hide columns with no items.

--- src/js/components/List.jsx
+++ src/js/components/List.jsx
@@ -3,15 +3,17 @@ import React, { PureComponent } from 'react';
 class List extends PureComponent {
     render() {
         return (
-            <div className="list">
-                <h2 className="list__header">
-                    {this.props.name}
-                    <span className="list__header_item_count">
-                        {this.props.children.length}
-                    </span>
-                </h2>
-                <ul className="list__list">{this.props.children}</ul>
-            </div>
+            this.props.children.length > 0 && (
+                <div className="list">
+                    <h2 className="list__header">
+                        {this.props.name}
+                        <span className="list__header_item_count">
+                            {this.props.children.length}
+                        </span>
+                    </h2>
+                    <ul className="list__list">{this.props.children}</ul>
+                </div>
+            )
         );
     }
 }
