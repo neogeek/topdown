4b2145dc3d5c227082120b2767a6d51229eefa2a
Scott Doxey <neogeek0101@gmail.com>
2018-08-01 17:27:53 -0400

    Pull from network first for non primary cache files.

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -58,7 +58,7 @@ self.addEventListener('fetch', event => {
                     .open(cacheVersion)
                     .then(cache => cache.add(event.request.url));
 
-                return response || fetch(event.request);
+                return fetch(event.request).catch(() => response);
             })
         );
     }
